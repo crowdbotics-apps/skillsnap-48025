1cbcfdf284201933e8b9ee1cf7f878398e85b1d7
Crowdbotics <team@crowdbotics.com>
2024-05-20 16:55:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skillsnap-48025",
+  url: "https://skillsnap-48025.botics.co",
+  api: "https://skillsnap-48025.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
